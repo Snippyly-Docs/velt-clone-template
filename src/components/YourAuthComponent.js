9cecefce1d458dbd468597fa81c635ea543bdfcf
benjvelt <benjamin@velt.dev>
2024-04-02 13:43:01 -0700

    g

--- src/components/YourAuthComponent.js
+++ src/components/YourAuthComponent.js
@@ -4,91 +4,86 @@ import { useEffect, useState } from "react";
 export default function YourAuthComponent() {
   let [user, setUser] = useState(null);
 
-  const userService = () => {
-    let randomNum = Math.floor(Math.random() * 10);
-    const names = [
-        "Michael Scott",
-        "Dwight Schrute",
-        "Jim Halpert",
-        "Pam Beesly",
-        "Ryan Howard",
-        "Andy Bernard",
-        "Robert California",
-        "Stanley Hudson",
-        "Kevin Malone",
-        "Meredith Palmer",
-      ];
-      
+  // Get the Velt Client
+  const { client } = useVeltClient();
+
+  const names = [
+    "Michael Scott",
+    "Dwight Schrute",
+    "Jim Halpert",
+    "Pam Beesly",
+    "Ryan Howard",
+    "Andy Bernard",
+    "Robert California",
+    "Stanley Hudson",
+    "Kevin Malone",
+    "Meredith Palmer",
+  ];
+
+  const generateUser = (randomNum, visibility = null) => {
+    const email = names[randomNum].split(" ")[0] + names[randomNum].split(" ")[1] + "@velt.com";
+    const photoURL = "https://i.pravatar.cc/30" + randomNum + `?u=user${randomNum}@velt.dev`;
+
     return {
       uid: "user" + randomNum,
       displayName: names[randomNum],
-      email: names[randomNum].split(" ")[0] + names[randomNum].split(" ")[1] + "@trysnippyly.com",
-      photoURL: "https://i.pravatar.cc/30" + randomNum + `?u=user${randomNum}@velt.dev`
+      email: email,
+      photoURL: photoURL,
+      ...(visibility && { visibility: visibility })
     };
   };
 
+  const userService = () => {
+    let user;
+    if (!localStorage.getItem("user")) {
+      let randomNum = Math.floor(Math.random() * 10);
+      const generatedUser = generateUser(randomNum);
+      const { uid, displayName, email, photoURL } = generatedUser;
+      user = {
+        userId: uid,
+        name: displayName,
+        email: email,
+        photoUrl: photoURL,
+        groupId: "theoffice",
+      };
+      user.contacts = [
+        getContact(0),
+        getContact(1),
+        getContact(2),
+        getContact(3),
+        getContact(4),
+        getContact(5),
+        getContact(6),
+        getContact(7),
+        getContact(8),
+        getContact(9)
+      ]
+      localStorage.setItem("user", JSON.stringify(user));
+    } else {
+      user = JSON.parse(localStorage.getItem("user"));
+    }
+    return user;
+  };
+
   const getContact = (num) => {
-    let randomNum = num
-    const names = [
-        "Michael Scott",
-        "Dwight Schrute",
-        "Jim Halpert",
-        "Pam Beesly",
-        "Ryan Howard",
-        "Andy Bernard",
-        "Robert California",
-        "Stanley Hudson",
-        "Kevin Malone",
-        "Meredith Palmer",
-      ];
-      
-    return {
-      userId: "user" + randomNum,
-      name: names[randomNum],
-      email: names[randomNum].split(" ")[0] + names[randomNum].split(" ")[1] + "@gmail.com",
-      photoURL: "https://i.pravatar.cc/30" + randomNum + `?u=user${randomNum}@velt.dev`,
-      visibility: "group"
-    };
+    return generateUser(num, "group");
   };
 
   // Fetch user data from user service
   let yourAuthenticatedUser = userService();
 
-  // Get the Velt Client
-  const { client } = useVeltClient();
+
+  const initVelt = async () => {
+    if (client && yourAuthenticatedUser) {
+
+      // Identify the user with the Velt client
+      await client.identify(yourAuthenticatedUser);
+      setUser(yourAuthenticatedUser);
+    }
+  };
 
   useEffect(() => {
-    const initVelt = async () => {
-      if (client && yourAuthenticatedUser) {
-        const { uid, displayName, email, photoURL } = yourAuthenticatedUser;
-
-        // Create the Velt user object
-        let user = {
-          userId: uid,
-          name: displayName,
-          email: email,
-          photoUrl: photoURL,
-          groupId: "theoffice",
-        };
-        user.contacts = [
-            getContact(0),
-            getContact(1),
-            getContact(2),
-            getContact(3),
-            getContact(4),
-            getContact(5),
-            getContact(6),
-            getContact(7),
-            getContact(8),
-            getContact(9)
-          ]
-    
-        // Identify the user with the Velt client
-        await client.identify(user);
-        setUser(user);
-      }
-    };
-    initVelt().catch(console.error);
+    initVelt();
   }, [client]);
 
   return <div></div>;
--- src/components/YourDocument.js
+++ src/components/YourDocument.js
@@ -12,15 +12,27 @@ export default function YourDocument() {
     if (client) {
       // Set a document ID
       client.setDocumentId('unique-demo-id');
+
+      //Set Dark Mode
+      //client.setDarkMode(true)
+
+      //Enable Live Selection
       const selectionElement = client.getSelectionElement();
       selectionElement.enableLiveSelection();
+
+
+      const commentElement = client.getCommentElement();
+      //Remove border on cell comments
+      //commentElement.disableCommentPinHighlighter();
+      //Remove dialog on Hover
+      //commentElement.disableDialogOnHover();
 
     }
   }, [client]);
 
   return (
    <div style={{ display:"flex", gap:"8px", marginRight:"16px"}}>
-     <VeltPresence flockMode={true}/>
+     <VeltPresence self={true} flockMode={true}/>
      <VeltHuddleTool/>
      <VeltCommentTool/>
      <VeltSidebarButton/>
